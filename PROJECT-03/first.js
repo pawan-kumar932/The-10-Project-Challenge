7d998c5ee843efabca61196c4b1e5df858989f62
pawan-kumar932 <pawankumarbella123@gmail.com>
2026-04-02 13:48:15 +0530

    some modify code

--- PROJECT-03/first.js
+++ PROJECT-03/first.js
@@ -9,15 +9,21 @@ form.addEventListener('submit',(e)=>{
 
     const res = Math.pow(l1+l2,3)%101;
     document.getElementById('result');
-    if(res < 13){
-        result.textContent = `Result:${res}% Try Again`;
-    }
-    else if(res > 80){
-        result.textContent = `Result:${res}% Perfect Match`;
-    }
-    else{
-        result.textContent = `Result:${res}%`;
-    }
+    if (res < 30) {
+    // Kam score ke liye
+    result.textContent = `Result: ${res}% - Keep looking!`;
+    result.style.color = "red"; 
+} 
+    else if (res > 80) {
+    // High score ke liye
+    result.textContent = `Result: ${res}% - Perfect Match! `;
+    result.style.color = "green";
+} 
+    else {
+    // Average score ke liye
+    result.textContent = `Result: ${res}% - There's a spark!`;
+    result.style.color = "orange";
+}
     
     form.reset();
 
